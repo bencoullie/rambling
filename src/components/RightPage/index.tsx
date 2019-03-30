0c2f26dcbd5b84e3eb24c806a09e96fcfa2111aa
Benjamin Coullie <ben.coullie@vendhq.com>
2019-03-30 21:50:36 +1300

    add right page component

---
+++ src/components/RightPage/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles.css'
+
+const RightPage = () => {
+	return (
+		<section className="page page--right-side">
+			<div className="page__content">
+				<p>
+					I know I should have written sooner. It’s just there’s been things upon things. Pilings of must dos and should dos. Anyway. It’s winter here. Yesterday it snowed. I’ve seen snow — the kind that looks like shredded cloud — but this was different. It was tired. This town seemed put down. Like some family pet let go. Hell, it even had the the steel grey frame, frosted in that heady white fluorescence you only really find in operating rooms. You know the light, basically smells of disinfectant and piss. The town just shut up. It was just a whole nother thing. I think this place, you’d like it.
+				</p>
+				<p>No need to reply.</p>
+			</div>
+		</section>
+	)
+}
+
+export default RightPage
