5311ce3945ef5498e13990a9d864e69c5e7396b3
Benjamin Coullie <ben.coullie@vendhq.com>
2019-03-30 21:50:27 +1300

    add left page component

---
+++ src/components/LeftPage/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.css'
+
+const LeftPage = () => {
+	return (
+		<section className="page page--left-side">
+			<div className="page__content">
+				<p>
+					Tiny, shimmering white lines on dark, black flecked green surrounded by a brown grey brown of sometimes blown dust and sand. Clouds fat to thin, panting from a mouth that hasn’t closed in minutes. Leaning down, the glove on his right hand pulled up exposing a pink yellow thumb to the morning whip of an asian winter sun, he readies contact — about to smudge the whitened leaf — ice?
+				</p>
+			</div>
+		</section>
+	)
+}
+
+export default LeftPage
