be47186b32dc1e1fc1c35abf76dc70a9d5b61715
Benjamin Coullie <ben.coullie@vendhq.com>
2019-03-31 20:43:46 +1300

    add redux remote dev tools

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,7 @@
 import { createStore } from 'redux'
+import { devToolsEnhancer } from 'redux-devtools-extension'
 import initialReducer from './reducers/initialReducer'
 
-const store = createStore(initialReducer)
+const store = createStore(initialReducer, devToolsEnhancer({ name: 'Rambling' }))
 
 export default store
