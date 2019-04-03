9b7a8a12e6bd5b73f96815025620e84e8e012262
Benjamin Coullie <ben.coullie@vendhq.com>
2019-04-03 20:53:42 +1300

    adapt store to handle sagas

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,17 @@
-import { createStore } from 'redux'
-import { devToolsEnhancer } from 'redux-devtools-extension'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import initialReducer from './reducers/initialReducer'
+import createSagaMiddleware from 'redux-saga'
+import fetchStoriesSaga from './sagas/fetchStoriesSaga'
 
-const store = createStore(initialReducer, devToolsEnhancer({ name: 'Rambling' }))
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(initialReducer, composeWithDevTools(
+	applyMiddleware(sagaMiddleware)
+))
+
+// then run the saga
+sagaMiddleware.run(fetchStoriesSaga)
 
 export default store
