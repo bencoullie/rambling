aaca90b39a899a0bb83957cc04decbb1e4e2143d
Benjamin Coullie <ben.coullie@vendhq.com>
2019-04-03 20:58:20 +1300

    add fetch stories function

---
+++ src/utilities/fetchStories/index.ts
@@ -0,0 +1,14 @@
+import Parser from 'rss-parser'
+import parseApiStories from './parseApiStories'
+
+let parser = new Parser();
+
+const fetchStories = async () => {
+	const rssFeedUrl = 'https://cors-anywhere.herokuapp.com/https://medium.com/feed/@benjamincoullie'
+	const jsonRssFeed = await parser.parseURL(rssFeedUrl);
+	const stories = jsonRssFeed.items
+
+	return stories && stories.length ? parseApiStories(stories.reverse()) : []
+}
+
+export default fetchStories
