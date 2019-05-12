3625b871dd69f1e1093fc59d7bedc31126f585e0
Benjamin Coullie <ben.coullie@vendhq.com>
2019-05-12 20:13:54 +1200

    ensure stupid typescript knows the action will always be visibility type

--- src/state/reducers/storiesReducer/index.ts
+++ src/state/reducers/storiesReducer/index.ts
@@ -1,6 +1,7 @@
 import storyAction, {
   STORIES_FETCH_SUCCEEDED_ACTION_TYPE,
   UPDATE_STORY_VISIBILITY_ACTION_TYPE,
+  UpdateStoryType,
 } from '../../actions/storyAction'
 import { Story } from '../../../types/story'
 import hydrationReducer from './hydrationReducer'
@@ -14,7 +15,9 @@ const storiesReducer = (existingStories: Story[] = [], action: storyAction) => {
       return hydrationReducer(stories)
 
     case UPDATE_STORY_VISIBILITY_ACTION_TYPE:
-      return visibilityReducer(existingStories, action)
+      return visibilityReducer(existingStories, action as storyAction<
+        UpdateStoryType
+      >)
 
     default:
       return existingStories
